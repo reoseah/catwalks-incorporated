5cf283257a480faa52da22ccf14a22554e6657b4
Reoseah <reoseah9@gmail.com>
2021-12-13 18:22:59 -0800

    Reference sound events to ensure static fields are initialized

--- src/main/java/com/github/reoseah/catwalksinc/CatwalksInc.java
+++ src/main/java/com/github/reoseah/catwalksinc/CatwalksInc.java
@@ -6,14 +6,6 @@ import org.apache.logging.log4j.Logger;
 import net.fabricmc.api.ModInitializer;
 import net.minecraft.util.Identifier;
 
-/**
- * FIXME Fix forced handrails desyncing after rejoining a world
- *
- * TODO disable Iron Rod recipe if Modern Industrialization is present
- * 
- * TODO add melting recipes for stuff back into iron
- * - possible more efficient recipes if tech mods are available
- */
 public class CatwalksInc implements ModInitializer {
 	public static final String MODID = "catwalksinc";
 
@@ -29,5 +21,6 @@ public class CatwalksInc implements ModInitializer {
 		CIItems.CATWALK.getClass();
 		CIBlocks.BlockEntityTypes.CATWALK.getClass();
 		CIRecipeSerializers.PAINTROLLER_FILLING.getClass();
+		CISounds.WRENCH_USE.getClass();
 	}
 }
