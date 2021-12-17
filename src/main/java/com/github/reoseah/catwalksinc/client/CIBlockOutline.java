3771052d82c0fa83ec22113df4833b7311d33e85
Reoseah <reoseah9@gmail.com>
2021-12-17 22:00:30 +0100

    Update CIBlockOutline.java

--- src/main/java/com/github/reoseah/catwalksinc/client/CIBlockOutline.java
+++ src/main/java/com/github/reoseah/catwalksinc/client/CIBlockOutline.java
@@ -14,11 +14,9 @@ import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.render.RenderLayer;
 import net.minecraft.client.render.VertexConsumer;
 import net.minecraft.client.util.math.MatrixStack;
-import net.minecraft.client.util.math.Vector3d;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
 import net.minecraft.util.math.MathHelper;
-import net.minecraft.util.math.Quaternion;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.util.math.Vec3f;
 
