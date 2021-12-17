1e3542048412dde2bf72417db16c1e616c3f5194
Reoseah <reoseah9@gmail.com>
2021-12-17 09:07:54 +0100

    Change Paint Roller REI description, show it for all filled paint rollers too

--- src/main/java/com/github/reoseah/catwalksinc/rei/CatwalksIncREI.java
+++ src/main/java/com/github/reoseah/catwalksinc/rei/CatwalksIncREI.java
@@ -11,12 +11,18 @@ import me.shedaniel.rei.api.client.registry.display.DisplayRegistry;
 import me.shedaniel.rei.api.client.registry.display.DynamicDisplayGenerator;
 import me.shedaniel.rei.api.common.entry.EntryStack;
 import me.shedaniel.rei.plugin.common.displays.DefaultInformationDisplay;
+import net.fabricmc.fabric.api.tag.TagFactory;
 import net.minecraft.client.resource.language.I18n;
+import net.minecraft.item.Item;
+import net.minecraft.tag.Tag;
 import net.minecraft.text.TranslatableText;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.Util;
+import net.minecraft.util.registry.Registry;
 
 public class CatwalksIncREI implements REIClientPlugin {
+	private static final Tag<Item> PAINT_ROLLERS = TagFactory.ITEM.create(CatwalksInc.id("filled_paint_rollers"));
+
 	@Override
 	public void registerDisplays(DisplayRegistry registry) {
 		registry.registerGlobalDisplayGenerator(new DynamicDisplayGenerator<DefaultInformationDisplay>() {
@@ -38,6 +44,19 @@ public class CatwalksIncREI implements REIClientPlugin {
 						.filter(id -> id.getNamespace().equals(CatwalksInc.MODID)) //
 						.map(id -> Util.createTranslationKey("misc",
 								new Identifier(id.getNamespace(), id.getPath() + ".recipe"))) //
+						.filter(I18n::hasTranslation) //
+						.map(info -> DefaultInformationDisplay.createFromEntry(entry, entry.asFormatStrippedText())
+								.line(new TranslatableText(info)))
+						.map(ImmutableList::of);
+			}
+		});
+		registry.registerGlobalDisplayGenerator(new DynamicDisplayGenerator<DefaultInformationDisplay>() {
+			@Override
+			public Optional<List<DefaultInformationDisplay>> getUsageFor(EntryStack<?> entry) {
+				return Optional.ofNullable(entry.getIdentifier()) //
+						.filter(id -> PAINT_ROLLERS.contains(Registry.ITEM.get(id))) //
+						.map(id -> Util.createTranslationKey("misc",
+								new Identifier(id.getNamespace(), "paint_roller.usage"))) //
 						.filter(I18n::hasTranslation) //
 						.map(info -> DefaultInformationDisplay.createFromEntry(entry, entry.asFormatStrippedText())
 								.line(new TranslatableText(info)))
