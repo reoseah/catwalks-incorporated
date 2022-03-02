fc7f115026d498ef2586ce8b9d25f808046c4f82
Reoseah <reoseah9@gmail.com>
2022-03-02 23:36:10 +0100

    Update to 1.18.2

--- src/main/java/com/github/reoseah/catwalksinc/rei/CatwalksIncREI.java
+++ src/main/java/com/github/reoseah/catwalksinc/rei/CatwalksIncREI.java
@@ -7,10 +7,10 @@ import me.shedaniel.rei.api.client.registry.display.DisplayRegistry;
 import me.shedaniel.rei.api.client.registry.display.DynamicDisplayGenerator;
 import me.shedaniel.rei.api.common.entry.EntryStack;
 import me.shedaniel.rei.plugin.common.displays.DefaultInformationDisplay;
-import net.fabricmc.fabric.api.tag.TagFactory;
 import net.minecraft.client.resource.language.I18n;
 import net.minecraft.item.Item;
-import net.minecraft.tag.Tag;
+import net.minecraft.item.ItemStack;
+import net.minecraft.tag.TagKey;
 import net.minecraft.text.TranslatableText;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.Util;
@@ -20,7 +20,7 @@ import java.util.List;
 import java.util.Optional;
 
 public class CatwalksIncREI implements REIClientPlugin {
-	private static final Tag<Item> PAINT_ROLLERS = TagFactory.ITEM.create(CatwalksInc.id("filled_paint_rollers"));
+	private static final TagKey<Item> PAINT_ROLLERS = TagKey.of(Registry.ITEM_KEY, CatwalksInc.id("filled_paint_rollers"));
 
 	@Override
 	public void registerDisplays(DisplayRegistry registry) {
@@ -53,7 +53,7 @@ public class CatwalksIncREI implements REIClientPlugin {
 			@Override
 			public Optional<List<DefaultInformationDisplay>> getUsageFor(EntryStack<?> entry) {
 				return Optional.ofNullable(entry.getIdentifier()) //
-						.filter(id -> PAINT_ROLLERS.contains(Registry.ITEM.get(id))) //
+						.filter(id -> new ItemStack(Registry.ITEM.get(id)).isIn(PAINT_ROLLERS)) //
 						.map(id -> Util.createTranslationKey("misc",
 								new Identifier(id.getNamespace(), "paint_roller.usage"))) //
 						.filter(I18n::hasTranslation) //
